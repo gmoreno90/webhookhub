d52af496ed6a4475cbb97d62c03679f025b546b0
Gonzalo Moreno <gonzalo.moreno@polaris.es>
2020-12-28 12:31:49 +0100

    mod Startup

--- WebHookHub/Startup.cs
+++ WebHookHub/Startup.cs
@@ -144,6 +144,7 @@ namespace WebHookHub
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseDeveloperExceptionPage();
             //app.Use((context, next) =>
             //{
             //    context.Request.EnableBuffering();
